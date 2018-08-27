370c60544f9e089f8d359b4099a8e87baefabb70
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-27 20:55:04 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   
   def add
     raise params.inspect
-    cart << params[:item]
+    cart << params[:product]
     
     session[:cart] = cart
     
